60154ba0ea6901e4777fd4c94e203a87a2d6afb5
Mikhail Golovko <mihailmvg@gmail.com>
2025-04-09 16:16:05 +0300

    Bump version

--- Sources/SwiftGen/Version.swift
+++ Sources/SwiftGen/Version.swift
@@ -5,8 +5,8 @@
 //
 
 enum Version {
-  static let swiftgen = "6.6.3"
-  static let swiftGenKit = "6.6.3"
+  static let swiftgen = "6.6.5"
+  static let swiftGenKit = "6.6.5"
   static let stencil = "0.15.1"
   static let stencilSwiftKit = "2.10.1"
 }
